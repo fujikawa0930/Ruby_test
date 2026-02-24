e31f2503b1b6d5a9d64fcc6a9d75fb55409155ed
fujikawa0930 <245630105+fujikawa0930@users.noreply.github.com>
2026-02-24 22:10:18 +0900

    Ruby 練習

--- test.rb
+++ test.rb
@@ -1,9 +1,6 @@
-orange = "Ehime"
+dice = 0
 
-if orange == "Yamaguchi"
-  puts "このみかんは山口県産です。"
-elsif orange == "Ehime"
-  puts "このみかんは山口県産ではなく、愛媛県産です。"
-else
-  puts "このみかんは山口県産でも愛媛県産でもありません。"
+while dice != 6 do
+  dice = rand(1..6)
+  puts dice
 end
