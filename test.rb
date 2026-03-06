7dabbc1655c127930b2ad1d9b74bfcee949e6cbc
fujikawa0930 <245630105+fujikawa0930@users.noreply.github.com>
2026-03-06 23:56:48 +0900

    Ruby 練習

--- test.rb
+++ test.rb
@@ -10,19 +10,7 @@ amounts.each do |fruit, amount|
   puts "#{fruit}は#{amount}個です。"
 end
 
-hand = "パー"
 
-if hand == "パー"
-  puts "出した手はパーです"
-end
-
-if hand != "パー"
-  puts "出した手はパーではありません"
-end
-
-if (hand == "グー") || (hand == "パー")
-  puts "出した手はグーまたはパーです"
-end
 
 hand = "パー"
 
