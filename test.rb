c1d560233252165e7f1ba7facce1fe33977ab817
fujikawa0930 <245630105+fujikawa0930@users.noreply.github.com>
2026-03-17 22:17:36 +0900

    Ruby 練習

--- test.rb
+++ test.rb
@@ -28,3 +28,32 @@ amounts.each do |fruit, amount|
   puts "#{fruit}は#{amount}個です。"
 end
 
+def say_hello
+  puts "hello"
+end
+
+say_hello
+
+amounts = {"ブルーベリー"=>10, "メロン"=>3, "イチゴ"=>6}
+
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です。"
+end
+
+amounts = {"ブルーベリー"=>10, "メロン"=>3, "イチゴ"=>6}
+
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です。"
+end
+
+amounts = {"ブルーベリー"=>10, "メロン"=>3, "イチゴ"=>6}
+
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です。"
+end
+
+amounts = {"ブルーベリー"=>10, "メロン"=>3, "イチゴ"=>6}
+
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です。"
+end
